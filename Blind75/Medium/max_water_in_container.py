3188320d2424cb510cccdddb6bbdf9837cbc4e56
Arka Patra <arkapatra2009@gmail.com>
2026-03-22 23:19:36 +0530

    Use two-pointer algorithm for maxArea
    Replace incorrect approach that picked two largest heights and squared them with the standard two-pointer solution for the Container With Most Water problem. The new implementation runs in O(n) time and O(1) space, iterating from both ends and updating the max area until pointers meet. This fixes wrong results from the previous heuristic.

--- Blind75/Medium/max_water_in_container.py
+++ Blind75/Medium/max_water_in_container.py
@@ -2,19 +2,15 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        maxNum = 0
-        secondNum = 0
-        for i in range(len(height)):
-            if maxNum < height[i]:
-                maxNum = height[i]
-            
-            if height[i] > secondNum and height[i] < maxNum:
-                secondNum = height[i]
-            
-        if secondNum:
-            return secondNum**2
-        else:
-            return maxNum**2
+        left, right = 0, len(height)-1
+        area = 0
+        while left < right:
+            area = max(area, min(height[left], height[right])*(right-left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return area
 
 if __name__ == "__main__":
     height = [4,3,2,1,4]
