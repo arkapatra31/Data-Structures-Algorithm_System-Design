252553cc5e7f1964ee7a3b98306eeb9d15f0f115
arkapatra31 <arkapatra2009@gmail.com>
2026-06-22 00:00:39 +0530

    Refactor MinStack methods for improved readability and consistency

--- Leetcode/Blind75/Medium/min_stack.py
+++ Leetcode/Blind75/Medium/min_stack.py
@@ -6,24 +6,21 @@ class MinStack:
         self.stack = []
         self.min_stack = []
 
-    def push(self, value: int) -> None:
-        self.stack.append(value)
-        if not self.min_stack or value <= self.min_stack[-1]:
-            self.min_stack.append(value)
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.min_stack) == 0 or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
 
     def pop(self) -> None:
-        if self.stack:
-            top_value = self.stack.pop()
-            if top_value == self.min_stack[-1]:
-                self.min_stack.pop()
-        
+        val = self.stack.pop()
+        if val == self.getMin():
+            self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1] if self.stack else None
-        
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min_stack[-1] if self.min_stack else None
+        return self.min_stack[-1]
         
 
 
